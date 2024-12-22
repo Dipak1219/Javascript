9e7e5b2d4ec1ccc10f4b98d919fdf6d348693378
It's D. <166849007+Dipak1219@users.noreply.github.com>
2024-12-22 10:14:22 +0000

    Extra Added

--- first.js
+++ first.js
@@ -1,1 +1,10 @@
 console.log("My first program in JS!");
+
+let MyName = "Dipak";
+let Age = 19;
+let Address = "Kaganari";
+
+console.table([ MyName,Age,Address]);
+
+// Primitive Datatypes :-  Bigint,Boolean,symbol,number,string,null,undefined
+// Non-Primirive Datatypes :- Array,object,function
